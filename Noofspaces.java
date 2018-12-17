3ba6735c959fa2469a5e36e0ac35671f4bc574a5
avism49 <avism49@gmail.com>
2018-12-17 14:18:32 -0500

    spaces program to take i/p staterment

---
+++ Noofspaces.java
@@ -0,0 +1,24 @@
+package assignmts;
+
+import java.util.Scanner;
+
+public class Noofspaces {
+	
+	public static void main(String[] args)
+	{
+		Scanner scn = new Scanner(System.in);
+
+		System.out.print("Enter User Text: ");
+
+		String text = scn.nextLine();
+		 
+		String data[];int k=0;
+	        data=text.split("");
+	        for(int i=0;i<data.length;i++)
+	        {
+	            if(data[i].equals(" "))
+	                k++;
+            }
+	        System.out.println(k);
+	  }
+	                    }
